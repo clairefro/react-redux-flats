90cdd13648dbde2e4edfaef1605cac5c8bf98692
claire froelich <claire.froelich@gmail.com>
2020-01-09 18:10:49 -0500

    add

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
 
 import App from './components/app';
 
@@ -19,10 +20,12 @@ const reducers = combineReducers({
   marker: markerReducer
 });
 
+const middlewares = applyMiddleware(logger);
+
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStore(reducers, {}, middlewares)}>
       <App />
     </Provider>, root
   );
